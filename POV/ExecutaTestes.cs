aea154b2735ba32532cba826d458036a78e36760
rodrigostuqui <rst.monzani@gmail.com>
2022-12-21 19:46:14 -0300

    test: adicionando testes

---
+++ POV/ExecutaTestes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question4
+{
+    public class ExecutaTestes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(POV.funcaoRetornaQuantitadePOV(0.1M, 90));
+            Console.WriteLine(POV.funcaoRetornaQuantitadePOV(0.1M, 100));
+            Console.WriteLine(POV.funcaoRetornaQuantitadePOV(0.2M, 70));
+        }
+    }
+}
